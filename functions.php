f5ac91acf572199f7aac1afdd6e710dc29de27c3
Sigafoos <dan.j.conley@gmail.com>
2014-01-27 14:35:49 -0500

    It will grab the leaderboards for my friends list and only grab BGGWWers. Ultimately it'd be good to keep looping until everyone in the group has been checked, but for now, you need to be my friend.

--- functions.php
+++ functions.php
@@ -16,29 +16,37 @@ function check_today($player) {
 }
 
 // Retrieves the player's leaderboard data for today
-function get_leaderboard_data($player, $leaderboard) {
-	$steamid = $player->steamid;
+function get_leaderboard_data($members, $leaderboard) {
+	//$steamid = $player->steamid; // I have their id
 	$score = -1;
 
-	$player_spelunky_leaderboard = 'http://steamcommunity.com/stats/239350/leaderboards/' . $leaderboard . '/?xml=1&steamid=' . $steamid;
+	// using my id, then grabbing the scores for anyone in the group
+	$player_spelunky_leaderboard = 'http://steamcommunity.com/stats/239350/leaderboards/' . $leaderboard . '/?xml=1&steamid=76561198000338942';
 
 	$xml = file_get_contents($player_spelunky_leaderboard);
 	$ob = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
 	$json = json_encode($ob);
 	$array = json_decode($json, true);
+	/*
+	   echo "<pre>";
+	   print_r($array);
+	   echo "</pre>";
+	   */
 
 	foreach($array['entries']['entry'] as $key => $value) {
-		if ($value['steamid'] == $steamid) {
-			$player->score = $value['score'];
+		if (in_array($value['steamid'],$members)) {
+			$scores[$value['steamid']]['score'] = $value['score'];
 
 			// Characters are stored as hex values, converting to decimal
-			$player->character = hexdec(substr($value['details'], 0, 2));
+			$scores[$value['steamid']]['character'] = hexdec(substr($value['details'], 0, 2));
 
 			// Levels ares stored as hex values from 0-19, so convert them into what's shown on the leaderboards in-game
 			$level = hexdec(substr($value['details'], 8, 2));
-			$player->level = ceil($level / 4) . "-" . ($level % 4 == 0? 4 : ($level % 4));
+			$scores[$value['steamid']]['level'] = ceil($level / 4) . "-" . ($level % 4 == 0? 4 : ($level % 4));
 		}
 	}
+
+	return $scores;
 }
 
 // Find the id for today's daily challenge leaderboard
@@ -62,6 +70,16 @@ function get_todays_leaderboard() {
 	return $leaderboard;
 }
 
+// grab the users in a group (easiest way to get everybody in the BGGWW community)
+function get_group_members($group) {
+	$xml = file_get_contents("http://steamcommunity.com/gid/" . $group . "/memberslistxml/?xml=1");
+	$ob = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+	$json = json_encode($ob);
+	$array = json_decode($json, TRUE);
+
+	return $array['members']['steamID64'];
+}
+
 // I'll have to do work on this, so, uh, it doesn't exist
 function get_youtube() {
 }
@@ -73,3 +91,30 @@ function post_leaderboard() {
 // I'll have to do work on this, so, uh, it doesn't exist
 function store_leaderboard() {
 }
+
+function character_icon($id) {
+	$colors = array(
+			'orange',
+			'red',
+			'green',
+			'blue',
+			'white',
+			'pink',
+			'yellow',
+			'brown',
+			'purple',
+			'black',
+			'cyan',
+			'lime',
+			'dlc1',
+			'dlc2',
+			'dlc3',
+			'dlc4',
+			'dlc5',
+			'dlc7',
+			'dlc8'
+		       );
+
+	// stuff
+}
+?>
--- index.php
+++ index.php
@@ -8,7 +8,19 @@ require('functions.php');
 </head>
 <body>
 <?php
+// get the BGGWW members
+$members = get_group_members("103582791432200102");
 
+// get the current leaderboard
+$leaderboard_id = get_todays_leaderboard();
+
+$leaderboard = get_leaderboard_data($members,$leaderboard_id);
+
+// IF THERE ARE PEOPLE IN THE GROUP NOT PRESENT, CHECK AGAIN?
+
+echo "<pre>";
+print_r($leaderboard);
+echo "</pre>";
 ?>
 </body>
 </html>

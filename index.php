b15cc1966125430f0a974b9ea4f77a51e1f68584
Sigafoos <dan.j.conley@gmail.com>
2014-01-29 15:51:44 -0500

    Index displays the date, and I fixed a formatting bug

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ if (!$_GET['date']) $_GET['date'] = date("Y-m-d");
 <!DOCTYPE html>
 <html>
 <head>
-<title>Spelunky!</title>
+<title>Spelunky: <?php echo date("F j, Y",strtotime($_GET['date'])); ?>!</title>
 </head>
 <body>
-<h1>Spelunky!</h1>
+<h1>Spelunky: <?php echo date("F j, Y",strtotime($_GET['date'])); ?>!</h1>
 <?php
 echo "<p><a href=\"?date=" . date("Y-m-d",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
 if (strtotime($_GET['date']) >= strtotime(date("Y-m-d",strtotime("now")))) echo "next day";
-else echo "<p><a href=\?date=" . date("Y-m-d",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
+else echo "<a href=\"?date=" . date("Y-m-d",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
 echo "</p>";
  
 // get the current leaderboard

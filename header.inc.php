6abf37c57f1b3f38916e046ef57e3ddba874e7fb
Sigafoos <dan.j.conley@gmail.com>
2014-02-04 12:31:07 -0500

    Added header/footer files, css and the start of a design

---
+++ header.inc.php
@@ -0,0 +1,15 @@
+<?php
+require('functions.php');
+if (!$title) $title = "Spelunky!";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $title; ?>!</title>
+<script type="text/javascript" src="/css/modernizr.js"></script>
+<link href="/css/style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<h1><?php echo $title; ?></h1>
+<div id="wrapper">
+
--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-require('functions.php');
 if (!$_GET['date']) $_GET['date'] = date("Y-m-d");
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Spelunky: <?php echo date("F j, Y",strtotime($_GET['date'])); ?>!</title>
-</head>
-<body>
-<h1>Spelunky: <?php echo date("F j, Y",strtotime($_GET['date'])); ?>!</h1>
-<?php
-echo "<p><a href=\"?date=" . date("Y-m-d",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
+$title = date("F j, Y",strtotime($_GET['date']));
+require('header.inc.php');
+
+echo "<p class=\"center\"><a href=\"?date=" . date("Y-m-d",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
 if (strtotime($_GET['date']) >= strtotime(date("Y-m-d",strtotime("now")))) echo "next day";
 else echo "<a href=\"?date=" . date("Y-m-d",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
 echo "</p>";
@@ -19,6 +12,6 @@ echo "</p>";
 $leaderboard_id = get_leaderboard($_GET['date']);
 $leaderboard = get_saved_leaderboard($leaderboard_id);
 print_leaderboard($leaderboard);
+
+require('footer.inc.php');
 ?>
-</body>
-</html>

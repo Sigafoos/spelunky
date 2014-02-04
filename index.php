ebeead51955e72113f8c5bf3fd4c3cc7f629098a
Sigafoos <dan.j.conley@gmail.com>
2014-02-04 12:49:56 -0500

    Switching to html5 nav element, some more styling. Still a waaaays to go.

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ if (!$_GET['date']) $_GET['date'] = date("Y-m-d");
 $title = date("F j, Y",strtotime($_GET['date']));
 require('header.inc.php');
 
-echo "<p class=\"center\"><a href=\"?date=" . date("Y-m-d",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
+echo "<nav><a href=\"?date=" . date("Y-m-d",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
 if (strtotime($_GET['date']) >= strtotime(date("Y-m-d",strtotime("now")))) echo "next day";
 else echo "<a href=\"?date=" . date("Y-m-d",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
-echo "</p>";
+echo "</nav>";
  
 // get the current leaderboard
 $leaderboard_id = get_leaderboard($_GET['date']);

7b26610034dd0cc6bcd8f95101fcbe5ca6180008
Sigafoos <dan.j.conley@gmail.com>
2014-02-06 19:15:59 -0500

    Start the new day at 7 pm EST, but keep checking old games til 9

--- cron.php
+++ cron.php
@@ -5,9 +5,18 @@ require('functions.php');
 $members = get_group_members("103582791432200102");
 
 // get the current leaderboard
-$leaderboard_id = get_leaderboard();
-$leaderboard = get_leaderboard_data($members,$leaderboard_id);
+// if it's after 9 EST, stop
+if (date("G") < 20) {
+	$leaderboard_id = get_leaderboard();
+	$leaderboard = get_leaderboard_data($members,$leaderboard_id);
+	save_leaderboard($leaderboard, $leaderboard_id);
+}
 
-if (save_leaderboard($leaderboard, $leaderboard_id)) update_leaderboard($leaderboard);
+// if it's after 7 pm EST
+if (date("G") > 18) {
+	$leaderboard_id = get_leaderboard(date("m/d/Y",strtotime("tomorrow")));
+	$leaderboard = get_leaderboard_data($members,$leaderboard_id);
+	save_leaderboard($leaderboard, $leaderboard_id);
+}
 echo "All shiny, cap'n"; // turn this off or pipe the output into /dev/null
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-if (!$_GET['date']) $_GET['date'] = date("Y-m-d");
+if (!$_GET['date']) {
+	if (date("G") < 19) $_GET['date'] = date("Y-m-d");
+	else $_GET['date'] = date("Y-m-d",strtotime("tomorrow"));
+}
 $title = date("F j, Y",strtotime($_GET['date']));
 require('header.inc.php');
 
 echo "<nav><a href=\"" . date("/Y/m/d/",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
-if (strtotime($_GET['date']) >= strtotime(date("Y-m-d",strtotime("now")))) echo "next day";
-else echo "<a href=\"" . date("/Y/m/d/",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
+// if it's before today, or it's today AND after 7 pm
+if ((strtotime($_GET['date']) < strtotime(date("Y-m-d",strtotime("now")))) || (strtotime(date("Y-m-d")) == strtotime($_GET['date']) && date("G") > 18)) echo "<a href=\"" . date("/Y/m/d/",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
+else echo "next day";
 echo "</nav>";
  
 // get the current leaderboard

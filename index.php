3ebbf191e7bd8c0cb5d7a7a6007067ff2a67fafc
Sigafoos <dan.j.conley@gmail.com>
2014-02-13 18:41:24 -0500

    link to global stats

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (!$_GET['date']) {
 $title = date("F j, Y",strtotime($_GET['date']));
 require('header.inc.php');
 
-echo "<nav><a href=\"" . date("/Y/m/d/",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | ";
+echo "<nav><a href=\"" . date("/Y/m/d/",strtotime("-1 day",strtotime($_GET['date']))) . "\">previous day</a> | <a href=\"/stats/\">statistics</a> | ";
 // if it's before today, or it's today AND after 7 pm
 if ((strtotime($_GET['date']) < strtotime(date("Y-m-d",strtotime("now")))) || (strtotime(date("Y-m-d")) == strtotime($_GET['date']) && date("G") > 18)) echo "<a href=\"" . date("/Y/m/d/",strtotime("+1 day",strtotime($_GET['date']))) . "\">next day</a>";
 else echo "next day";
